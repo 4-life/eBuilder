af2e4636523fa4de25a9aa2f9ad678dd2d5636c4
4life <pavel@ovchinnikov.us>
2017-10-19 23:21:21 +0300

    Imagemin fix

--- tasks/prepareImgCommonRelease.js
+++ tasks/prepareImgCommonRelease.js
@@ -2,18 +2,13 @@ const config = global.config;
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const newer = require('gulp-newer');
-const pngquant = require('imagemin-pngquant');
 
 module.exports = function() {
   return gulp.src(config.sourceDir + 'img/**/*.{png,jpeg,jpg}')
     .pipe(newer(config.tempDir + 'img'))
     .pipe(imagemin([
-        imagemin.jpegtran({progressive: true, arithmetic: true}),
-        pngquant({quality:60}),
-      ],{
-      verbose: true,
-      progressive: true,
-      interlaced: true,
-    }))
+      imagemin.jpegtran({progressive: true, arithmetic: true}),
+      imagemin.optipng({optimizationLevel: 5})
+    ]))
     .pipe(gulp.dest(config.tempDir + 'img'));
 };
--- tasks/prepareImgCustomRelease.js
+++ tasks/prepareImgCustomRelease.js
@@ -2,18 +2,15 @@ const config = global.config;
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const newer = require('gulp-newer');
-const pngquant = require('imagemin-pngquant');
 
 module.exports = function() {
   return gulp.src(config.sourceDir + '_images/**/*.{png,jpeg,jpg}')
     .pipe(newer(config.tempDir))
     .pipe(imagemin([
-        imagemin.jpegtran({progressive: true, arithmetic: true}),
-        pngquant({quality:60}),
-      ],{
-      verbose: true,
-      progressive: true,
-      interlaced: true,
+      imagemin.jpegtran({progressive: true, arithmetic: true}),
+      imagemin.optipng({optimizationLevel: 5})
+    ],{
+      verbose: true
     }))
     .pipe(gulp.dest(config.tempDir));
 };
